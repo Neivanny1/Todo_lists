060f58907d3b84ff767b6f3df1322bba7357c6b1
Neivanny1 <nevileneivanny@gmail.com>
2024-02-14 23:27:05 +0300

    more styles

--- Todo list1/main.js
+++ Todo list1/main.js
@@ -14,10 +14,10 @@ window.addEventListener('load', () => {
         // creating a div to embed in html
         const task_el = document.createElement('div');
         task_el.classList.add('task');
-        
+        // create div with class content
         const task_content_el = document.createElement('div');
         task_content_el.classList.add('content');
-
+        // append div inside taskel with main div
         task_el.appendChild(task_content_el);
 
         const task_input_el = document.createElement('input');
@@ -27,9 +27,37 @@ window.addEventListener('load', () => {
         task_input_el.setAttribute('readonly', 'readonly');
 
         task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
+
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerHTML = 'Edit';
+
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerHTML ='Delete';
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+        task_el.appendChild(task_actions_el);
         list_el.appendChild(task_el);
-        
-        task_el.appendChild(task_content_el);
-        list_el.appendChild(task_el);
-    })
-})
+        input.value = '';
+        // editing and saving 
+        task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+        // deeting
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
